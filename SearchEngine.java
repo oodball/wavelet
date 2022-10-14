5d8bb7729827fa6ecbe480918532757b7d4d180d
oodball <74622107+oodball@users.noreply.github.com>
2022-10-13 20:12:14 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,38 +1,37 @@
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
-// import java.util.*;
+
 class Handler implements URLHandler {
 
     int num = 0;
     ArrayList<String> listRequest = new ArrayList<String>();
 
     public String handleRequest(URI url) {
+        String finalOutput = "";
         if (url.getPath().equals("/")) {
             return String.format("This is the Search Engine Homepage");
-        } else if (url.getPath().equals("/add")) {
+        } 
+        else if (url.getPath().equals("/add")) {
             String[] parameters = url.getQuery().split("=");
-            listRequest.add(parameters[1]);
+            if (parameters[0].equals("s")) {
+                listRequest.add(parameters[1]);
+                return String.format("Adding... ", listRequest.get(listRequest.size() - 1 ));
+                }   
+            }
+        else if (url.getPath().equals("/search")) {
+            String[] parameters = url.getQuery().split("=");
                 if (parameters[0].equals("s")) {
-                    return String.format("Adding... ", listRequest.get(listRequest.size() - 1 ));
-                }
-            }else if (url.getPath().equals("/search")) {
-                String[] parameters = url.getQuery().split("=");
-                    if (parameters[0].equals("s")) {
-                        for(String s: listRequest){
-                            if(s.contains(parameters[1])){
-                                return s;
-                            }
-                            return String.format("Searching for: ", parameters[1]);
+                    for(String s: listRequest){
+                        if(s.contains(parameters[1])){
+                            finalOutput = finalOutput + " " + s;
                         }
                     }
+                    return finalOutput;
                 }
+            }
             return String.format("We couldn't quite find what you were looking for.");
         } 
-        // else {
-        //     System.out.println("Path: " + url.getPath());
-        // }
-        //     return "404 Not Found!";
     }
 
 class SearchEngine {
